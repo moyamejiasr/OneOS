dd74f890c4a5eed65324a19c41ad9502983098bc
dgrr <dgrripoll@gmail.com>
2018-10-28 22:33:11 +0100

    Formatted

--- src/kmain.c
+++ src/kmain.c
@@ -15,53 +15,51 @@
 #define FB_ROW                  5
 #define FB_COLUMNS              80
 
-enum Color {    Black, Blue, Green, Cyan,
-                Red, Magenta, Brown, Light_grey,
-                Dark_grey, Light_blue, Light_green, Light_cyan,
-                Light_red, Light_magenta, Light_brown, White 
-            };
+enum Color {
+  Black, Blue, Green, Cyan,
+  Red, Magenta, Brown, Light_grey,
+  Dark_grey, Light_blue, Light_green, Light_cyan,
+  Light_red, Light_magenta, Light_brown, White 
+};
 
 unsigned short fb_r = 0, fb_c = 0;
 
 char *fb = (char *)FB_POS;
 
-/** fb_move_cursor:
- *  Moves the cursor of the framebuffer to the given position
- *
- *  @param pos The new position of the cursor
- */
-void fb_move_cursor(unsigned short pos)
+// Moves the cursor of the framebuffer to the given position
+void
+fb_move_cursor(unsigned short pos)
 {
-    outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
-    outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
-    outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
-    outb(FB_DATA_PORT,    pos & 0x00FF);
+  outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+  outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
+  outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+  outb(FB_DATA_PORT,    pos & 0x00FF);
 }
 
-/** fb_print_chars:
- *  Prints defined chars and move the cursor to the next position.
- *
- *  @param c A char array with the string to type(must end in \0)
- *  @param fg Foreground color from enum
- *  @param bg Background color from enum
- */
-void fb_print_chars(char* c, unsigned char fg, unsigned char bg)
+// Prints defined chars and move the cursor to the next position.
+//
+// c is char array with the string to type(must end in \0)
+// fg is the foreground color from enum
+// bg is the background color from enum
+void
+fb_print_chars(char* c, unsigned char fg, unsigned char bg)
 {
-    unsigned int i = 0;
-    while ( c[i] != 0 ) {
-        if (fb_c >= FB_COLUMNS || c[i] == '\n') {
-            fb_c = 0;
-            fb_r++;
-            //continue;
-        }
-        fb[fb_c++ + (fb_r * FB_COLUMNS)] = c[i++];
-        fb[fb_c++ + (fb_r * FB_COLUMNS)] = fg | bg << 4;
-        fb_move_cursor(fb_c + (fb_r * FB_COLUMNS) + 1);
+  unsigned int i = 0;
+  while ( c[i] != 0 ) {
+    if (fb_c >= FB_COLUMNS || c[i] == '\n') {
+        fb_c = 0;
+        fb_r++;
+        //continue;
     }
+    fb[fb_c++ + (fb_r * FB_COLUMNS)] = c[i++];
+    fb[fb_c++ + (fb_r * FB_COLUMNS)] = fg | bg << 4;
+    fb_move_cursor(fb_c + (fb_r * FB_COLUMNS) + 1);
+  }
 }
 
-void krnl_main()
+void
+krnl_main()
 {
-    fb_print_chars("OneOS init.\nTesting line jump~~\0", White, Black);
-    //fb_move_cursor(80);
+  fb_print_chars("OneOS init.\nTesting line jump~~\0", White, Black);
+  //fb_move_cursor(80);
 }
