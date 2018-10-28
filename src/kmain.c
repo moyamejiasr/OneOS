ed66e5cb96b35d8b438813a31494e589dc72c384
Onelio <rmoya98@gmail.com>
2018-10-28 09:53:37 +0100

    Added C Code

---
+++ src/kmain.c
@@ -0,0 +1,6 @@
+    
+    /* The C function */
+    int krnl_main(int arg1, int arg2, int arg3)
+    {
+        return arg1 + arg2 + arg3;
+    }
--- src/loader.s
+++ src/loader.s
@@ -12,6 +12,24 @@
         dd CHECKSUM                 ; and the checksum
 
     loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+        mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+                                                    ; stack (end of memory area)
+
+        ; The assembly code for C convention
+        extern krnl_main   ; the function krnl_main is defined elsewhere
+
+        push dword 3            ; arg3
+        push dword 2            ; arg2
+        push dword 1            ; arg1
+        call krnl_main       ; call the function, the result will be in eax
+
     .loop:
         jmp .loop                   ; loop forever
+
+
+    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+
+    section .bss:
+    align 4                         ; align at 4 bytes
+    kernel_stack:                   ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
